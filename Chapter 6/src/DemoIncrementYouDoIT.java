b7ec9a3d46761750432a6889a47d7cebc5edb3b9
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-06-29 20:46:41 +0500

    Add files via upload

---
+++ Chapter 6/src/DemoIncrementYouDoIT.java
@@ -0,0 +1,13 @@
+public class DemoIncrementYouDoIT {
+    public static void main(String[] args){
+       int v = 4;
+       int vplusPlus = ++v;
+
+       v = 4;
+       int vPlusplus = v++;
+
+       System.out.print(" v is "+ v);
+        System.out.print("\n++v is "+ vplusPlus);
+        System.out.print("\nv++ is " + vPlusplus);
+    }
+}
