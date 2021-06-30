415d93904ff95db2f2b4243866e392a522ab3acd
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-06-30 23:53:17 +0500

    Add files via upload

---
+++ Chapter 6/src/TablesOfSquare.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int number;
+for(number = 1; number <= 20; number++){
+int square = number * number;
+cout << "the square of " << number << " is = "<< square << endl;
+}
+return 0;
+}
