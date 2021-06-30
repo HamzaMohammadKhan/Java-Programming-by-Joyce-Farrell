d132cb1423ddddb68f4b2afc30a117029ed6c9e3
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-06-30 23:39:23 +0500

    Add files via upload

---
+++ Chapter 6/src/Input123.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int number;
+cout<< "Enter your number "<<endl;
+cin>>number;
+for(;number < 4;){
+cout<<"Good Job"<<endl;
+break;
+if(number == 4){
+            cout<< " " << endl;
+}if(number > 4){
+cout<<" Error!!!" << endl;
+   }
+  }
+  return 0 ;
+}
+
+
+
