d46fce4192603b669d3dc74c58a12c893f053379
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-07-14 23:30:03 +0500

    Exercise in c++

---
+++ Chapter 7/JoinString.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+  std::string a1 = "Event Handlers is dedicated ";
+  std::string b1 = " to maing your event ";
+  std::string c1 = " a most memorable one ";
+
+  cout << a1 + b1 + c1 << endl;
+  return 0;
+}
