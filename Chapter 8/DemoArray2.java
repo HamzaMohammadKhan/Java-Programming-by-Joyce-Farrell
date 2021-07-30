d76801dfa4edfab2c7d678582f6307e995fe874d
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-07-30 20:50:01 +0500

    You do it

---
+++ Chapter 8/DemoArray2.java
@@ -0,0 +1,11 @@
+public class DemoArray2 {
+    public static void main(String[] args){
+        double[] salary = {6.25, 6.55, 10.25, 16.85};
+
+        System.out.println("PRint Salary one by one");
+        System.out.println(salary[0]);
+        System.out.println(salary[1]);
+        System.out.println(salary[2]);
+        System.out.println(salary[3]);
+    }
+}
