d4611027e38184c1adb32d8256f6680d0a31e9c3
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-08-04 22:57:42 +0500

    Exercise

---
+++ Chapter 8/CollegeCourse.java
@@ -0,0 +1,20 @@
+public class CollegeCourse {
+    int courseID;
+    int creditHours;
+    char grade;
+
+
+    int getCourseID(){
+        return courseID;
+    }
+    void setCourseID(int ID){
+        courseID = ID;
+    }
+
+    int getCreditHours(){
+        return creditHours;
+    }
+    void setCreditHours(int hr){
+        creditHours = hr;
+    }
+}
