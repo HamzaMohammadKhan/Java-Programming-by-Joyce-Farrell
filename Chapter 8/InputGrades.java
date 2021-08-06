f973661894fa38b57a91ef452d26ad7f6d5542c3
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-08-06 23:56:23 +0500

    Incomplete exercise

---
+++ Chapter 8/InputGrades.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class InputGrades {
+    public static void main(String[] args){
+        Student[] arr = new Student[10];
+        Student obj1 = new Student();
+        Scanner sc = new Scanner(System.in);
+        for(int s=0; s<arr.length; s++){
+            System.out.println("Enter ID for student #s");
+            int id = sc.nextInt();
+            
+        }
+    }
+}
