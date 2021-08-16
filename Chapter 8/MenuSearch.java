14e03df383c5b98b8d3a87039e8d726f17871ac1
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-08-16 23:40:16 +0500

    Add files via upload

---
+++ Chapter 8/MenuSearch.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import javax.swing.*;
+
+public class MenuSearch {
+    public static void main(String[] args){
+        String[] menuChoices = new String[10];
+        String entry = " ", menuString = " ";
+        int x=0;
+        int numEnetred;
+        int highestSub = menuChoices.length -1;
+        Arrays.fill(menuChoices, "ZZZZZZZ");
+
+    }
+}
