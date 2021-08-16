7d1096eee8538d7b05f661222c4e54c06892734c
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-08-17 00:14:15 +0500

    You do it

---
+++ Chapter 8/MenuSearch.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import javax.swing.*;
+
+public class MenuSearch {
+    public static void main(String[] args){
+        String[] menuChoices = new String[10];
+        String entry = " ", menuString = " ";
+        int x=0;
+        int numEntered;
+        int highestSub = menuChoices.length -1;
+        Arrays.fill(menuChoices, "ZZZZZZZ");
+        menuChoices[x] = JOptionPane.showInputDialog(null,"Enter an item for today's menu, or zzz to quit: ");
+        while(!menuChoices[x].equals("zzz") && x < highestSub){
+            menuString = menuString + menuChoices[x] + "\n";
+            ++x;
+            if(x < highestSub){
+                menuChoices[x] = JOptionPane.showInputDialog(null,"Enter an item for today's menu, or zzz to quit");
+            }
+            numEntered = x;
+            entry = JOptionPane.showInputDialog(null, " Today's menu is: \n" + menuString + "Please make a selection: ");
+            Arrays.sort(menuChoices, 0, numEntered);
+            x = Arrays.binarySearch(menuChoices, entry);
+            if(x >= 0 && x < numEntered){
+                JOptionPane.showMessageDialog(null," Sorry - that utem is not on tonight' menu");
+            }
+        }
+        System.exit(0);
+    }
+}
