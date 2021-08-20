7b32a1f80496245f0fcbbffc181ea5fdabeada1a
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-08-20 20:42:03 +0500

    Exercise
    Complete

---
+++ Chapter 8/StudentIDArray.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.util.Arrays;
+
+public class StudentIDArray {
+    public static void main(String[] args){
+        final int TOTAL = 5;
+        int[] Id = {111,222,333,444,555};
+        double[] gpa = {2.42,3.0,4,3.5,2.8};
+        String[] names = {"hamza","ali","umair","aqib","shah"};
+        String stuID, student = "";
+        boolean valid = false;
+        double cgpa = 0.0;
+        int x;
+        stuID = JOptionPane.showInputDialog(null," enter ID : ");
+        int stuID1 = Integer.parseInt(stuID);
+        for(x=0; x < TOTAL; ++x){
+            for(x=0; x< names.length; ++x)
+            if(stuID1 == Id[x])
+            {
+                student = names[x];
+                valid = true;
+                cgpa = gpa[x];
+            }
+        }
+        if(valid){
+            JOptionPane.showMessageDialog(null, " The student is : " + student + "\n" + " Roll number : " + stuID1 + "\n" + " marks : " + cgpa);
+        }else{
+            JOptionPane.showMessageDialog(null,"wrong entry");
+        }
+    }
+
+}
