2488a49772872d157bb1ba3da30ae894efc47782
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-09-04 21:07:29 +0500

    Add files via upload

---
+++ Chapter 11/src/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    private int empNum;
+    private double empSal;
+
+    public int getEmpNum(){
+        return empNum;
+    }
+    public void setEmpNum(int num){
+        empNum = num;
+    }
+
+    public double getEmpSal(){
+        return empSal;
+    }
+    public void setEmpSal(double sal){
+        empSal = sal;
+    }
+}
