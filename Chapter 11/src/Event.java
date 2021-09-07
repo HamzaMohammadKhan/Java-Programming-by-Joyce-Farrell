24d5edceaeea8555a92fec2d1a1e283a19ba05ef
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-09-07 21:14:52 +0500

    You do it

---
+++ Chapter 11/src/Event.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Event {
+    private int eventGuests;
+    public void displayGuestsEvents(){
+        JOptionPane.showMessageDialog(null, "Event guests: " + eventGuests);
+    }
+    public void setEventGuests(){
+        char inChar;
+        String guestString =  new String();
+        guestString = JOptionPane.showInputDialog(null,"Enter the number of guests at event ");
+        eventGuests = Integer.parseInt(guestString);
+    }
+}
---
+++ Chapter 11/src/UseSimpleEvent.java
@@ -0,0 +1,8 @@
+public class UseSimpleEvent {
+    public static void main(String[] args){
+        Event obj1 = new Event();
+        obj1.setEventGuests();
+        obj1.displayGuestsEvents();
+        System.exit(0);
+    }
+}
