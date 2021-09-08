c83e5e969230c1ffb2aca6ef6a24e6abe5723470
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-09-08 21:05:34 +0500

    Update UseSimpleEvent.java

--- Chapter 11/src/UseSimpleEvent.java
+++ Chapter 11/src/UseSimpleEvent.java
@@ -1,8 +1,17 @@
 public class UseSimpleEvent {
-    public static void main(String[] args){
+        public static void main(String[] args){
         Event obj1 = new Event();
+        JOptionPane.showMessageDialog(null,"Creating an event");
         obj1.setEventGuests();
         obj1.displayGuestsEvents();
+
+        DinnerEvent obj2 = new DinnerEvent();
+        obj2.setEventGuests();
+        obj2.setDinnerChoice();
+
+        obj2.displayDinnerChoice();
+        obj2.displayGuestsEvents();
         System.exit(0);
+    }
     }
 }
