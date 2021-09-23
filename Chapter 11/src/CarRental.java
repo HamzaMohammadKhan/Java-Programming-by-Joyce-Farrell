5c6c9cfb73bdd3427f23258af718e2a32fbe5829
Hamza Mohammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-09-24 00:34:11 +0500

    Exercise 7

---
+++ Chapter 11/src/CarRental.java
@@ -0,0 +1,13 @@
+public class CarRental {
+        private String RentalName;
+        private int zipCodeRented;
+        private int carSizeRented;
+        private double rentalFee;
+        private int rentalDays;
+        private double totalrentalFee;
+
+        CarRental(String Rn, int Zr, int Cr,double Rf){
+            RentalName = Rn;
+
+        }
+    }
