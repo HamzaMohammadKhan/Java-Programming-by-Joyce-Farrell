a324e8507d6be37658af404d682f42a5829b8e4b
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-11-12 14:57:41 -0500

    Exercise 9 ( incomplete)

---
+++ Chapter 11/src/UseVehicle.java
@@ -0,0 +1,8 @@
+public class UseVehicle {
+    public static void main(String[] args){
+        Car obj1 = new Car(12,3);
+        MotorCycle obj2 =new MotorCycle(11,4);
+        System.out.println(obj1);
+        System.out.println(obj2);
+    }
+}
