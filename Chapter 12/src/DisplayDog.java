65212aaf8487eceef81122c3ede310ce0501d18f
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-11-16 11:44:56 -0500

    Example

---
+++ Chapter 12/src/DisplayDog.java
@@ -0,0 +1,7 @@
+public class DisplayDog {
+    public static void main(String[] args){
+        Dog myDog = new Dog();
+        String dogString = myDog.toString();
+        System.out.println(dogString);
+    }
+}
