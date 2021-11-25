2b7ea64d73920453614377b6bda37e347665ac97
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-11-25 22:08:25 +0500

    Update Client.java

--- Case Projects Joyce Farrell/src/Client.java
+++ Case Projects Joyce Farrell/src/Client.java
@@ -1,2 +1,38 @@
 public class Client {
+    int ClientNumber;
+    String firstName;
+    String lastName;
+    int primaryAttornyID;
+    double balance;
+
+    public int getClientNumber(){
+        return ClientNumber;
+    }
+    public void setClientNumber(int num){
+        ClientNumber = num;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String fn){
+        firstName = fn;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double bal){
+        balance = bal;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setLastName(String ln){
+        lastName = ln;
+    }
+    public int getPrimaryAttornyID(){
+        return primaryAttornyID;
+    }
+    public void setPrimaryAttornyID(int ID){
+        primaryAttornyID = ID;
+    }
 }
