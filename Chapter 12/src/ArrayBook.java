37d16202af55afa0ff419c02714be04459e6bcf9
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-12-07 18:43:38 +0500

    Question 1

---
+++ Chapter 12/src/ArrayBook.java
@@ -0,0 +1,10 @@
+public class ArrayBook {
+    public static void main(String[] args){
+        Book obj1[] = new Book[10];
+        NonFiction obj4 = new NonFiction();
+        String[] bookTitles = {"Halo", "Sekiro", "Dark Souls", "Dark souls 2", "Dark souls 3", "BloodBorne", "Demon Souls"};
+        for(int i=0; i< obj1.length; i++){
+
+        }
+    }
+}
---
+++ Chapter 12/src/NonFiction.java
@@ -0,0 +1,11 @@
+public class NonFiction extends Book{
+    public void setPrice(){
+        setBookPrice(37.99);
+    }
+    public void setName(){
+
+    }
+    public NonFiction(){
+        setPrice();
+    }
+}
