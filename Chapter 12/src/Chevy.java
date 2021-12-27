7b44a9ec4d290eddeda6c79987f3f5e55b2ea578
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-12-27 19:23:48 +0500

    Question 3

---
+++ Chapter 12/src/Chevy.java
@@ -0,0 +1,11 @@
+public class Chevy extends Auto{
+    @Override
+    public void setPrice() {
+        price = 22000;
+    }
+
+    @Override
+    public void setMake(String m) {
+        super.setMake(m);
+    }
+}
