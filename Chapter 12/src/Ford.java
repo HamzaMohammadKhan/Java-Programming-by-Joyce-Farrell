503b94f4db830eddc58ab9e9cbf034e58db7eed4
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2021-12-27 19:23:22 +0500

    Question 3

---
+++ Chapter 12/src/Ford.java
@@ -0,0 +1,10 @@
+public class Ford extends Auto{
+    @Override
+    public void setPrice() {
+        price = 200000;
+    }
+    @Override
+    public void setMake(String m) {
+        super.setMake(m);
+    }
+}
