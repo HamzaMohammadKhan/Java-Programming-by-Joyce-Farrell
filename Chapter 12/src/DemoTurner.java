be8138b3704fe3bdeba948a59fe346d23646c256
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2022-01-01 18:27:56 +0500

    Question 7

---
+++ Chapter 12/src/DemoTurner.java
@@ -0,0 +1,11 @@
+public class DemoTurner {
+    public static void main(String[] args){
+        Leaf obj1 = new Leaf();
+        Page obj2 = new Page();
+        Pancake obj3 = new Pancake();
+
+        obj1.turn();
+        obj2.turn();
+        obj3.turn();
+    }
+}
