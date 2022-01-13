9a523975659b5c57eaa29d99e40ba1c2bcbd45bc
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2022-01-13 20:17:50 +0500

    Chapter 13

---
+++ Chapter 13/src/MathMistake.java
@@ -0,0 +1,9 @@
+public class MathMistake {
+    public static void main(String[] args){
+        int num = 13, denom = 0, result;
+        if(denom == 0) {
+            System.exit(1);
+        }
+        result = num/denom;
+    }
+}
---
+++ Chapter 13/src/addition.java
@@ -0,0 +1,10 @@
+public class addition {
+        public static void main(String[] args) {
+            // Write your code here
+            int n=8;
+            for(int i=0; i <=10 ; i++){
+                int j = n*i;
+                System.out.print(n + "x" + i + "=" + j +"\n");
+            }
+        }
+    }
