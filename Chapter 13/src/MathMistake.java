f0327fa89478b80bad34dafd7fb779f24cad0a5b
Hamza Muhammad Khan <36361756+HamzaMohammadKhan@users.noreply.github.com>
2022-01-13 20:46:28 +0500

    Update MathMistake.java

--- Chapter 13/src/MathMistake.java
+++ Chapter 13/src/MathMistake.java
@@ -1,9 +1,11 @@
 public class MathMistake {
     public static void main(String[] args){
         int num = 13, denom = 0, result;
-        if(denom == 0) {
-            System.exit(1);
+        try{
+            result = num/denom;
         }
-        result = num/denom;
+        catch(ArithmeticException mistake){
+            System.out.println("Wrong division!");
+        }
     }
 }
